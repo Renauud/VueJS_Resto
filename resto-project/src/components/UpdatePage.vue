13355fe19f911ff7f6f93e880c2c51d0a1142be6
Renaud <rzell57@gmail.com>
2023-05-29 17:21:46 +0200

    fonction update faite, bouton update fonctionnel, il est maintenant possible de modifier un restaurant existant(son nom, adresse et son contact). Il reste à faire le suppression de restaurants

--- resto-project/src/components/UpdatePage.vue
+++ resto-project/src/components/UpdatePage.vue
@@ -5,7 +5,7 @@
         <input type="text" name="name" placeholder="Enter a restaurant name !" v-model="Restaurant.name">
         <input type="text" name="address" placeholder="Enter their address !" v-model="Restaurant.address">
         <input type="text" name="contact" placeholder="Enter their contact !" v-model="Restaurant.contact">
-        <button type="button" v-on:click="addRestaurant">Update the restaurant</button>
+        <button type="button" v-on:click="updateRestaurant">Update the restaurant</button>
     </form>
 </template>
 
@@ -26,6 +26,19 @@ export default{
             }
         }
     },
+    methods:{
+        async updateRestaurant()
+        {
+            const result = await axios.put("http://localhost:3000/restaurants/"+this.$route.params.id,{
+                name:this.Restaurant.name,
+                address:this.Restaurant.address,
+                contact:this.Restaurant.contact,
+            })
+            if(result.status === 200){
+                this.$router.push({name:"HomePage"})
+            }
+        }
+    },
     async mounted(){ //empêche l'utilisateur d'aller sur la page HomePage s'il n'est pas connecté
         let user = localStorage.getItem("user-info")
         if(!user){
@@ -34,7 +47,7 @@ export default{
         const result = await axios.get("http://localhost:3000/restaurants/" + this.$route.params.id)
         console.warn(result.data);
         this.Restaurant = result.data;
-    }
+    },
 }
 
 </script>
